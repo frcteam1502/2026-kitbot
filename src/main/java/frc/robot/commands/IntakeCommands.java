7891af57b2c16d4abab211976018b6cc435d3061
Hamgo272 <ke8uee@gmail.com>
2026-07-15 22:02:09 -0400

    create and setup intake commands

--- src/main/java/frc/robot/commands/IntakeCommands.java
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -2,7 +2,9 @@ package frc.robot.commands;
 
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.team1502.Driver;
 
 public class IntakeCommands extends Command {
     public final IntakeSubsystem m_subsystem;
@@ -14,7 +16,12 @@ public class IntakeCommands extends Command {
     @Override
     
     public void initialize(){
-        
+            Driver.B   
+            .onTrue(new InstantCommand(() -> m_subsystem.setIntakePower(0.4)))
+            .onFalse(new InstantCommand(() -> m_subsystem.setIntakePower(0)));
+            Driver.X
+            .onTrue(new InstantCommand(() -> m_subsystem.setIntakeLocation(0.1)))
+            .onFalse(new InstantCommand(() -> m_subsystem.setIntakeLocation(0)));
         }
 
     @Override
